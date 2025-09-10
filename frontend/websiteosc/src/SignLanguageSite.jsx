2839752993ff1d9844771129959e4d0e8a5d0d54
Yomna-Mahsoob <ymahsoob@gmail.com>
2025-09-11 01:11:34 +0300

    final finnalll

--- frontend/websiteosc/src/SignLanguageSite.jsx
+++ frontend/websiteosc/src/SignLanguageSite.jsx
@@ -490,7 +490,7 @@ export function TopNav({ active, setActive, dark, toggleDark }) {
           </Button>
 
           {currentUser && (
-            <div className="user-info flex flex-col items-center bg-gray-100 px-3 py-2 rounded-lg shadow-sm text-xs ml-2 flex-shrink-0">
+            <div className="user-info flex-col bg-gray-100 px-3 py-2 rounded-lg shadow-sm text-xs ml-2 flex-shrink-0">
               <User
                 size={28}
                 color="gray"
